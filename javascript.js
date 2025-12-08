88bd784d4ac98dd9c33affd07d9cf4e238e2e4a7
Alex <alexharris@live.co.uk>
2025-12-07 20:42:42 -0800

    Create event listener for keyboard

--- javascript.js
+++ javascript.js
@@ -1,13 +1,135 @@
-//Animate button on click
+// Event Delegation for keys
 
-document.addEventListener("mousedown", (event) => {
-    if (event.target.tagName === 'BUTTON') {
-        event.target.classList.add('moved');
+let input = document.getElementById("input");
+let keyboard = document.querySelector(".keyboard");
+
+keyboard.addEventListener('click', (event) => {
+    let target = event.target;
+    let inputLength = input.textContent.length;
+    console.log(inputLength);
+
+    switch(target.id) {
+        case "one":
+            if (input.textContent === "0") {
+                input.textContent = "1";
+                inputLength = 1;
+            }
+            else if (inputLength >= 10) {
+            }
+            else {
+                input.textContent += "1";
+            }
+            break;
+    }
+    switch(target.id) {
+        case "two":
+            if (input.textContent === "0") {
+                input.textContent = "2";
+            }
+            else if (inputLength >= 10) {
+            }
+            else {
+                input.textContent += "2";
+            }
+            break;
+    }
+    switch(target.id) {
+        case "three":
+            if (input.textContent === "0") {
+                input.textContent = "3";
+            }
+            else {
+                input.textContent += "3";
+            }
+            break;
+    }
+    switch(target.id) {
+        case "four":
+            if (input.textContent === "0") {
+                input.textContent = "4";
+            }
+            else {
+                input.textContent += "4";
+            }
+            break;
+    }
+    switch(target.id) {
+        case "five":
+            if (input.textContent === "0") {
+                input.textContent = "5";
+            }
+            else {
+                input.textContent += "5";
+            }
+            break;
+    }
+    switch(target.id) {
+        case "six":
+            if (input.textContent === "0") {
+                input.textContent = "6";
+            }
+            else {
+                input.textContent += "6";
+            }
+            break;
+    }
+    switch(target.id) {
+        case "seven":
+            if (input.textContent === "0") {
+                input.textContent = "7";
+            }
+            else {
+                input.textContent += "7";
+            }
+            break;
+    }
+    switch(target.id) {
+        case "eight":
+            if (input.textContent === "0") {
+                input.textContent = "8";
+            }
+            else {
+                input.textContent += "8";
+            }
+            break;
+    }
+    switch(target.id) {
+        case "nine":
+            if (input.textContent === "0") {
+                input.textContent = "9";
+            }
+            else {
+                input.textContent += "9";
+            }
+            break;
+    }
+    switch(target.id) {
+        case "zero":
+            if (input.textContent === "0") {
+                input.textContent = "0";
+            }
+            else {
+                input.textContent += "0";
+            }
+            break;
+    }
+    switch(target.id) {
+        case "clear":
+            input.textContent = "0";
+            break;
+    }
+    switch(target.id) {
+        case "delete":
+            if (inputLength === 1) {
+                console.log("Delete final number");
+                input.textContent = "0";
+            }
+            else {
+                console.log("Delete most recent");
+                input.textContent = input.textContent.slice(0, -1);
+            }
+            break;
     }
 })
 
-document.addEventListener("mouseup", (event) => {
-    document.querySelectorAll('button.moved').forEach(button => {
-    button.classList.remove('moved');
-    });
-})
+//Clear to 0
