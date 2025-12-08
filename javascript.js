6bc6933437f811ee4f5e74519ae0fab1ed318bb0
Alex <alexharris@live.co.uk>
2025-12-08 10:42:55 -0800

    Add event listeners to remaining buttons

--- javascript.js
+++ javascript.js
@@ -4,132 +4,283 @@ let input = document.getElementById("input");
 let keyboard = document.querySelector(".keyboard");
 
 keyboard.addEventListener('click', (event) => {
-    let target = event.target;
+    const btn = event.target.closest('button');
+    if (!btn) return;
     let inputLength = input.textContent.length;
-    console.log(inputLength);
 
-    switch(target.id) {
+    switch(btn.id) {
         case "one":
             if (input.textContent === "0") {
                 input.textContent = "1";
-                inputLength = 1;
             }
             else if (inputLength >= 10) {
+                break;
             }
             else {
                 input.textContent += "1";
             }
             break;
     }
-    switch(target.id) {
+    switch(btn.id) {
         case "two":
             if (input.textContent === "0") {
-                input.textContent = "2";
+                input.textContent = "2";         
             }
             else if (inputLength >= 10) {
+                break;
             }
             else {
                 input.textContent += "2";
             }
             break;
     }
-    switch(target.id) {
+    switch(btn.id) {
         case "three":
             if (input.textContent === "0") {
                 input.textContent = "3";
+            }
+            else if (inputLength >= 10) {
+                break;
             }
             else {
                 input.textContent += "3";
             }
             break;
     }
-    switch(target.id) {
+    switch(btn.id) {
         case "four":
             if (input.textContent === "0") {
                 input.textContent = "4";
+            }
+            else if (inputLength >= 10) {
+                break;
             }
             else {
                 input.textContent += "4";
             }
             break;
     }
-    switch(target.id) {
+    switch(btn.id) {
         case "five":
             if (input.textContent === "0") {
                 input.textContent = "5";
+            }
+            else if (inputLength >= 10) {
+                break;
             }
             else {
                 input.textContent += "5";
             }
             break;
     }
-    switch(target.id) {
+    switch(btn.id) {
         case "six":
             if (input.textContent === "0") {
                 input.textContent = "6";
+            }
+            else if (inputLength >= 10) {
+                break;
             }
             else {
                 input.textContent += "6";
             }
             break;
     }
-    switch(target.id) {
+    switch(btn.id) {
         case "seven":
             if (input.textContent === "0") {
                 input.textContent = "7";
+            }
+            else if (inputLength >= 10) {
+                break;
             }
             else {
                 input.textContent += "7";
             }
             break;
     }
-    switch(target.id) {
+    switch(btn.id) {
         case "eight":
             if (input.textContent === "0") {
                 input.textContent = "8";
+            }
+            else if (inputLength >= 10) {
+                break;
             }
             else {
                 input.textContent += "8";
             }
             break;
     }
-    switch(target.id) {
+    switch(btn.id) {
         case "nine":
             if (input.textContent === "0") {
                 input.textContent = "9";
+            }
+            else if (inputLength >= 10) {
+                break;
             }
             else {
                 input.textContent += "9";
             }
             break;
     }
-    switch(target.id) {
+    switch(btn.id) {
         case "zero":
             if (input.textContent === "0") {
-                input.textContent = "0";
+                break;
+            }
+            else if (inputLength >= 10) {
+                break;
             }
             else {
                 input.textContent += "0";
             }
             break;
     }
-    switch(target.id) {
+    switch(btn.id) {
         case "clear":
             input.textContent = "0";
             break;
     }
-    switch(target.id) {
+    switch(btn.id) {
         case "delete":
             if (inputLength === 1) {
-                console.log("Delete final number");
                 input.textContent = "0";
             }
             else {
-                console.log("Delete most recent");
                 input.textContent = input.textContent.slice(0, -1);
+            }
+            break;
+    }
+    switch(btn.id) {
+        case "decimal":
+            if (input.textContent === "0") {
+                input.textContent += "."
+            }
+            else if (inputLength >= 10) {
+                break;
+            }
+            else if (input.textContent.includes('.')) {
+                break;
+            }
+            else {
+                input.textContent += ".";
+            }
+            break;
+    }
+    switch(btn.id) {
+        case "add":
+            if (input.textContent === "0") {
+                input.textContent += "+";
+            }
+            else if (inputLength >= 10) {
+                break;
+            }
+            else if (input.textContent.includes('-')
+                || input.textContent.includes('+')
+                || input.textContent.includes('×') 
+                || input.textContent.includes('÷') 
+                || input.textContent.includes('%')) {
+                break;
+            }
+            else {
+                input.textContent += "+";
+            }
+            break;
+    }
+    switch(btn.id) {
+        case "minus":
+            if (input.textContent === "0") {
+                input.textContent += "-";
+            }
+            else if (inputLength >= 10) {
+                break;
+            }
+            else if (input.textContent.includes('-')
+                || input.textContent.includes('+')
+                || input.textContent.includes('×') 
+                || input.textContent.includes('÷') 
+                || input.textContent.includes('%')) {
+                break;
+            }
+            else {
+                input.textContent += "-";
+            }
+            break;
+    }
+    switch(btn.id) {
+        case "multiply":
+            if (input.textContent === "0") {
+                input.textContent+= '×';
+            }
+            else if (inputLength >= 10) {
+                break;
+            }
+            else if (input.textContent.includes('-')
+                || input.textContent.includes('+')
+                || input.textContent.includes('×') 
+                || input.textContent.includes('÷') 
+                || input.textContent.includes('%')) {
+                break;
+            }
+            else {
+                input.textContent += '×';
+            }
+            break;
+    }
+    switch(btn.id) {
+        case "divide":
+            if (input.textContent === "0") {
+                input.textContent += "÷";
+            }
+            else if (inputLength >= 10) {
+                break;
+            }
+            else if (input.textContent.includes('-')
+                || input.textContent.includes('+')
+                || input.textContent.includes('×') 
+                || input.textContent.includes('÷') 
+                || input.textContent.includes('%')) {
+                break;
+            }
+            else {
+                input.innerHTML += '÷';
+            }
+            break;
+    }
+    switch(btn.id) {
+        case "modulo":
+            if (input.textContent === "0") {
+                input.textContent += "%";
+            }
+            else if (inputLength >= 10) {
+                break;
+            }
+            else if (input.textContent.includes('-')
+                || input.textContent.includes('+')
+                || input.textContent.includes('×') 
+                || input.textContent.includes('÷') 
+                || input.textContent.includes('%')) {
+                break;
+            }
+            else {
+                input.textContent += "%";
+            }
+            break;
+    }
+    switch(btn.id) {
+        case "changeSign":
+            if (input.textContent === "0") {
+                break;
+            }
+            else if (input.textContent[0] === "-") {
+                input.textContent = input.textContent.slice(1);
+            }
+            else {
+                input.textContent = "-" + input.textContent ;
             }
             break;
     }
 })
 
-//Clear to 0
